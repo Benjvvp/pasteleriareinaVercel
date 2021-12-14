dfeb6e714db226119830f12f37edf22f6bd56180
_Benjvvp <BPerez51511a@gmail.com>
2021-12-14 16:50:05 -0300

    Add React-Ga

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
-
+import ReactGA from 'react-ga';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage, AboutPage, CakesPage, ContactPage } from "./Pages";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-FBYW99B5G1')
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
   return (
     <Router>
         <Routes>
